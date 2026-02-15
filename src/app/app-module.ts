99b5310e8b131352bf5c84ed3df5923ad972a571
Mateus Leandro <mateusleandro2205@gmail.com>
2026-02-15 10:38:57 -0300

    :sparkles:feat: cria componentes do projeto

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,18 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Header } from './shared/header/header';
+import { Footer } from './shared/footer/footer';
+import { Banner } from './shared/banner/banner';
+import { Card } from './shared/card/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [
-    App
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideBrowserGlobalErrorListeners()
-  ],
-  bootstrap: [App]
+  declarations: [App, Header, Footer, Banner, Card],
+  imports: [BrowserModule, AppRoutingModule, MatToolbarModule, MatButtonModule],
+  providers: [provideBrowserGlobalErrorListeners()],
+  bootstrap: [App],
 })
-export class AppModule { }
+export class AppModule {}
