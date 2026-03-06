c4ea52e51fd396d078504885eeb45960ec0cc65c
Mateus Leandro <mateusleandro2205@gmail.com>
2026-03-06 07:33:42 -0300

    :sparkles:feat: cria banner da aplicacao

--- src/app/shared/banner/banner.ts
+++ src/app/shared/banner/banner.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-banner',
@@ -7,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrl: './banner.scss',
 })
 export class Banner {
-
+  src = input<string>('');
+  alt = input<string>('');
 }
