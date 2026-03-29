dbcc5a93133401b5f550406d37cc3d9c6acb0bc1
Mateus Chagas <mateus.chagas@avancoinfo.com.br>
2026-03-29 15:57:50 -0300

    :bug:fix: utilizar signals para atualizar estada da lista

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { PromocaoService } from '../../core/services/promocao.service';
 import { Promocao } from '../../core/types/promocao';
 
@@ -9,13 +9,13 @@ import { Promocao } from '../../core/types/promocao';
   styleUrl: './home.scss',
 })
 export class Home implements OnInit {
-  promocoes: Promocao[] = [];
+  promocoes = signal<Promocao[]>([]);
 
   constructor(private servicoPromocao: PromocaoService) {}
 
   ngOnInit(): void {
     this.servicoPromocao.listar().subscribe((resposta) => {
-      this.promocoes = resposta;
+      this.promocoes.set(resposta);
     });
   }
 }
