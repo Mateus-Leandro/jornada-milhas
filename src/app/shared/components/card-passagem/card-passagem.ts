21128d588056844b67d30db8f1e3db0de221be58
Mateus Chagas <mateus.chagas@avancoinfo.com.br>
2026-05-25 21:57:27 -0300

    :lipstick:style: estilizacao card passagem

--- src/app/shared/components/card-passagem/card-passagem.ts
+++ src/app/shared/components/card-passagem/card-passagem.ts
@@ -6,8 +6,13 @@ import { Passagem } from '../../../core/types/passagens';
   standalone: false,
   templateUrl: './card-passagem.html',
   styleUrl: './card-passagem.scss',
-  
 })
 export class CardPassagem {
   @Input({ required: true }) passagem!: Passagem;
+  get textoIdaVolta() {
+    if (!this.passagem.dataVolta) {
+      return 'Somente ida';
+    }
+    return 'Ida e volta';
+  }
 }
